1441f6d53c56ac7555686da429cec5d52d47f0d8
mizuno58 <t.mizuno@g.dreamcareer.co.jp>
2022-08-25 11:02:48 +0900

    ログイン機能Next

--- components/image.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image'
-
-export default function MyImage(props){
-    let fname = './' + props.fname
-    let width = props.size + 'px'
-    return(
-        <img width={width}  border='1' src={fname} />
-    )
-}
