0495a89e70b0e3742362607610784c9cd9bad509
hikaru-ishikawa <hakodate123456789@gmail.com>
2022-09-09 14:34:36 +0900

    ログイン機能修正版

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,64 +1,31 @@
-import React, { useState } from "react"
 import { useRouter } from "next/router"
-import Link from "next/link"
-import { Alert, Button, InputLabel, Snackbar, TextField } from "@mui/material"
-//import { css } from "@emotion/react"
-import firebase from 'firebase/compat/app';
-import { getAuth, signInWithEmailAndPassword, getIdToken, setPersistence, browserSessionPersistence } from "firebase/auth"
-import { useAuthContext } from "./context/AuthContext"
+import { GoogleAuthProvider } from "firebase/auth";
+import { Alert, Button, Snackbar } from "@mui/material"
+import { getAuth, signInWithRedirect, setPersistence, browserSessionPersistence } from "firebase/auth"
 import { app } from "../components/fire/fire"
+import { useAuthContext } from "./context/AuthContext"
 
 const Login = () => {
+  const auth = getAuth(app)
   const { user } = useAuthContext()
   const isLoggedIn = !!user
   const router = useRouter()
-  const auth = getAuth(app)
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const provider = new GoogleAuthProvider();
+  const handleLogin = (e: { preventDefault: () => void; }) => {
     e.preventDefault()
-    await setPersistence(auth, browserSessionPersistence)
+    setPersistence(auth, browserSessionPersistence)
     .then(() => {
-      // Existing and future Auth states are now persisted in the current
-      // session only. Closing the window would clear any existing state even
-      // if a user forgets to sign out.
-      // ...
-      // New sign-in will be persisted with session persistence.
-      return signInWithEmailAndPassword(auth, email, password);
+     
+      return signInWithRedirect(auth, provider);
     })
-    .catch((error) => {
-      // Handle Errors here.
-      const errorCode = error.code;
-      const errorMessage = error.message;
-    });
-    //await signInWithEmailAndPassword(auth, email, password)
-    // .then(({user}) => {
-    //   const currentUser:firebase.User|null = firebase.auth().currentUser;
-    //     if (currentUser && !currentUser.emailVerified) {
-    //         firebase.auth().signOut();
-    //   }
-    // })
-    router.push("/")
-  }
-  const handleChangeEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setEmail(e.currentTarget.value)
-  }
-  const handleChangePassword = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.currentTarget.value)
+    router.push('/');
   }
   const handleClose = async () => {
     await router.push("/")
   }
 
   return (
-    <div
-    //   css={css`
-    //     display: flex;
-    //     justify-content: space-between;
-    //     align-items: center;
-    //     flex-flow: column;
-    //   `}
-    >
+    <div>
       <Snackbar
         open={isLoggedIn}
         anchorOrigin={{ vertical: "top", horizontal: "center" }}
@@ -69,7 +36,7 @@ const Login = () => {
         <Alert onClose={handleClose} severity="warning">
           すでにログインしています
         </Alert>
-      </Snackbar>
+      </Snackbar> 
       <Snackbar
         open={!isLoggedIn}
         anchorOrigin={{ vertical: "top", horizontal: "center" }}
@@ -78,72 +45,13 @@ const Login = () => {
       >
         <Alert severity="warning">ログインしてください</Alert>
       </Snackbar>
-      <h2>ログイン</h2>
-      <form onSubmit={handleSubmit}>
-        <div
-        //   css={css`
-        //     display: flex;
-        //     justify-content: center;
-        //     align-items: center;
-        //   `}
-        >
-          <InputLabel>メールアドレス</InputLabel>
-          <TextField
-            name="email"
-            type="email"
-            size="small"
-            onChange={handleChangeEmail}
-            // css={css`
-            //   padding-left: 12px;
-            // `}
-          />
-        </div>
-        <div
-        //   css={css`
-        //     display: flex;
-        //     justify-content: flex-end;
-        //     align-items: center;
-        //     margin-top: 16px;
-        //   `}
-        >
-          <InputLabel>パスワード</InputLabel>
-          <TextField
-            name="password"
-            type="password"
-            size="small"
-            onChange={handleChangePassword}
-            // css={css`
-            //   padding-left: 12px;
-            // `}
-          />
-        </div>
-        <div
-        //   css={css`
-        //     display: flex;
-        //     justify-content: flex-end;
-        //     margin-top: 16px;
-        //   `}
-        >
-          <Button type="submit" variant="outlined">
-            ログイン
-          </Button>
-        </div>
-        <div
-        //   css={css`
-        //     display: flex;
-        //     justify-content: flex-end;
-        //     margin-top: 24px;
-        //   `}
-        >
-          ユーザ登録は
-          <Link href={"/signup"}>
-            <a>こちら</a>
-          </Link>
-          から
-        </div>
-      </form>
+      <h2>Googleアカウント</h2>
+      <Button variant="outlined" onClick={handleLogin}>
+          ログイン
+      </Button>
     </div>
   )
 }
+
 
 export default Login
