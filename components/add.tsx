cc1740c5c88022b3c1ca882c731af9d463540849
Fukuhara0054 <t.fukuhara@g.dreamcareer.co.jp>
2022-09-26 12:54:32 +0900

    追加ボタン表示2

--- components/add.tsx
+++ components/add.tsx
@@ -120,7 +120,7 @@ const Add: FC<path> = ({ path }) => {
         // モーダルを閉じる処理を定義
         onRequestClose={closeModal}
         //React-modalのスタイルがデフォで当てられていたので下に記述
-        style={{
+        sx={{
           overlay: {
             position: "fixed",
             //MUIのz-indexが1100に設定されているため
